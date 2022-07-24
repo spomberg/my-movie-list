16e69e7038edce1dee0c55a22a1fefaa67348413
Marcos Spomberg <marcosspomberg@gmail.com>
2022-07-24 10:06:44 -0400

    Import useApplicationData

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import Navbar from './components/Navbar/Navbar';
+import Lists from './components/Lists/Lists';
+import useApplicationData from './hooks/useApplicationData';
 import './App.scss';
 
 function App() {
+  const state = useApplicationData();
+
   return (
     <div className='app'>
       <Navbar />
+      <Lists lists={state.lists} />
     </div>
   );
 }
