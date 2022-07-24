958eb5d2db9f791936b7ec9a588a0773a0d84a67
Marcos Spomberg <marcosspomberg@gmail.com>
2022-07-24 16:17:47 -0400

    Break Card into divs, fix username prop and remove description

--- src/components/Lists/ListItem.jsx
+++ src/components/Lists/ListItem.jsx
@@ -9,26 +9,31 @@ import './ListItem.scss';
 export default function ListItem(props) {
   return (
     <Card className='index-list-item' sx={{ maxWidth: 345 }}>
-      <CardContent>
-        <Typography variant="h6" component="div">
-          {props.title}
-        </Typography>
-        <Typography gutterBottom variant="subtitle1" component="div">
-          {props.created_by}
-        </Typography>
-        <Typography variant="body2" color="text.secondary">
-          {props.description}
-        </Typography>
-      </CardContent>
-      <CardActions>
-        <Button size="small">View List</Button>
-      </CardActions>
-      <CardMedia
-        component="img"
-        alt="list first movie poster"
-        height="140"
-        image={props.image}
-      />
+      <div className='list-item-left'>
+        <div className='left-top'>
+        <CardContent>
+          <Typography variant="h6" component="div">
+            {props.title}
+          </Typography>
+          <Typography gutterBottom variant="subtitle1" component="div">
+            Created by {props.username}
+          </Typography>
+        </CardContent>
+        </div>
+        <div className='left-bottom'>
+        <CardActions>
+          <Button size="small">View List</Button>
+        </CardActions>
+        </div>
+      </div>
+      <div className='list-item-right'>
+        <CardMedia
+          component="img"
+          alt="list first movie poster"
+          height="140"
+          image={props.image}
+        />
+      </div>
     </Card>
   )
 }
