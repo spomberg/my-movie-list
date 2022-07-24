bc0ffd0075a1c663cbcc783032ea5249c055b1bf
Marcos Spomberg <marcosspomberg@gmail.com>
2022-07-23 20:38:59 -0400

    Set up tabs and icons

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,8 +1,31 @@
 import React, { Component } from 'react';
 import './navbar.scss';
+import Tabs from '@mui/material/Tabs';
+import Tab from '@mui/material/Tab';
+import HomeIcon from '@mui/icons-material/Home';
+import FormatListNumberedIcon from '@mui/icons-material/FormatListNumbered';
+import PlaylistAddIcon from '@mui/icons-material/PlaylistAdd';
+import PersonIcon from '@mui/icons-material/Person';
+import PersonAddAltIcon from '@mui/icons-material/PersonAddAlt';
+import { useState } from 'react';
 
-export default class Navbar extends Component {
-  render() {
-    // return ()
-  }
+export default function Navbar() {
+  const [value, setValue] = useState(0);
+
+  const handleChange = (event, newValue) => {
+    setValue(newValue);
+  };
+
+    return (
+      <div className='navbar'>
+        <h1>My Movie List</h1> 
+        <Tabs value={value} onChange={handleChange} aria-label="icon label tabs">
+          <Tab icon={<HomeIcon />} label="VIEW LISTS" />
+          <Tab icon={<FormatListNumberedIcon />} label="MY LISTS" />
+          <Tab icon={<PlaylistAddIcon />} label="CREATE LIST" />
+          <Tab icon={<PersonIcon />} label="LOGIN" />
+          <Tab icon={<PersonAddAltIcon />} label="SIGNUP" />
+        </Tabs>
+      </div>
+    )
 }
