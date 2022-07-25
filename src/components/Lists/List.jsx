3cdd9ded58d7565da92511c23aa71aeebc2a2739
Marcos Spomberg <marcosspomberg@gmail.com>
2022-07-25 16:36:31 -0400

    Import list data hook

--- src/components/Lists/List.jsx
+++ src/components/Lists/List.jsx
@@ -1,6 +1,11 @@
 import './List.scss';
+import { useParams } from 'react-router-dom';
+import useListData from '../../hooks/useListData'
 
 export default function List() {
+  const params = useParams();
+  const list = useListData(params.listId).list
+
   return (
     <div>List</div>
   )
