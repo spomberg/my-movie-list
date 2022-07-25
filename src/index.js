1da9de94ed8aafcc96f940d3516a0de9fcbf8fbd
Marcos Spomberg <marcosspomberg@gmail.com>
2022-07-25 09:41:18 -0400

    Setup routes

--- src/index.js
+++ src/index.js
@@ -1,10 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+} from "react-router-dom";
 import App from './App';
+import Home from './components/Home/Home';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <BrowserRouter>
+  <Routes>
+    <Route path="/" element={<App />}>
+      <Route index element={<Home />} />
+    </Route>
+  </Routes>
+</BrowserRouter>
 );
