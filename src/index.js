072dc81e65a625360779e0e0c8fbf8e4278fe378
Marcos Spomberg <marcosspomberg@gmail.com>
2022-07-25 10:22:04 -0400

    Create :listId route

--- src/index.js
+++ src/index.js
@@ -4,17 +4,21 @@ import {
   BrowserRouter,
   Routes,
   Route,
-} from "react-router-dom";
+} from 'react-router-dom';
 import App from './App';
 import Home from './components/Home/Home';
+import List from './components/Lists/List';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <BrowserRouter>
-  <Routes>
-    <Route path="/" element={<App />}>
-      <Route index element={<Home />} />
-    </Route>
-  </Routes>
-</BrowserRouter>
+    <Routes>
+      <Route path="/" element={<App />}>
+        <Route index element={<Home />} />
+        <Route path="lists">
+          <Route path=":listId" element={<List />} />
+        </Route>
+      </Route>
+    </Routes>
+  </BrowserRouter>
 );
