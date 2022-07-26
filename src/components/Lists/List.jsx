890b9f1e3654a43f4b38f446c12da3e6e28f4cf0
Marcos Spomberg <marcosspomberg@gmail.com>
2022-07-26 12:55:13 -0400

    Import loader component and make it display for 1 second before showing page content

--- src/components/Lists/List.jsx
+++ src/components/Lists/List.jsx
@@ -4,24 +4,32 @@ import useListData from '../../hooks/useListData'
 import Box from '@mui/material/Box';
 import Card from '@mui/material/Card';
 import Typography from '@mui/material/Typography';
+import { BeatLoader } from 'react-spinners';
+import { useEffect, useState } from 'react';
 
 export default function List() {
+  const [loading, setLoading] = useState(true)
   const params = useParams();
   const list = useListData(params.listId).list
 
+  useEffect(() => {
+    setTimeout(() => setLoading(false), 1000)
+  }, [])
+
   return (
     <div className='list'>
-      <Box 
-        className='header'
-        component='span'
-        sx={{ display: 'inline-block', mx: '2px', transform: 'scale(0.8)' }}
-      >
-        <Card>
-          <Typography variant={'h3'}>{list.title}</Typography>
-          <Typography variant={'h6'} color="text.secondary">Created by {list.username}</Typography>
-          <Typography variant={'h5'}>Description: {list.description}</Typography>
-        </Card>
-    </Box>
+      {loading ? (<BeatLoader className='loader' loading={loading} />
+      ) : (<Box 
+              className='header'
+              component='span'
+              sx={{ display: 'inline-block', mx: '2px', transform: 'scale(0.8)' }}
+            >
+              <Card>
+                <Typography variant={'h3'}>{list.title}</Typography>
+                <Typography variant={'h6'} color="text.secondary">Created by {list.username}</Typography>
+                <Typography variant={'h5'}>Description: {list.description}</Typography>
+              </Card>
+          </Box>)}
     </div>
   )
 }
