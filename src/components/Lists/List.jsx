97f46af32ec7ad11c496dc826fb1c3f9280ab309
Marcos Spomberg <marcosspomberg@gmail.com>
2022-07-26 16:37:32 -0400

    Change setTimeout interval to 2000ms, import movieItem component

--- src/components/Lists/List.jsx
+++ src/components/Lists/List.jsx
@@ -4,6 +4,7 @@ import useListData from '../../hooks/useListData'
 import Box from '@mui/material/Box';
 import Card from '@mui/material/Card';
 import Typography from '@mui/material/Typography';
+import MovieItem from './MovieItem';
 import { BeatLoader } from 'react-spinners';
 import { useEffect, useState } from 'react';
 
@@ -13,23 +14,43 @@ export default function List() {
   const list = useListData(params.listId).list
 
   useEffect(() => {
-    setTimeout(() => setLoading(false), 1500)
+    setTimeout(() => setLoading(false), 2000)
   }, [])
 
   return (
     <div className='list'>
       {loading ? (<BeatLoader className='loader' loading={loading} />) : (
-        <Box 
-          className='header'
-          component='span'
-          sx={{ display: 'inline-block', mx: '2px', transform: 'scale(0.8)' }}
-        >
-          <Card>
-            <Typography variant={'h3'}>{list.title}</Typography>
-            <Typography variant={'h6'} color="text.secondary">Created by {list.username}</Typography>
-            <Typography variant={'h5'}>Description: {list.description}</Typography>
-          </Card>
-        </Box>
+        <>
+          <Box 
+            className='header'
+            component='span'
+            sx={{ display: 'inline-block', mx: '2px', transform: 'scale(0.8)' }}
+          >
+            <Card>
+              <Typography variant={'h3'}>{list.title}</Typography>
+              <Typography variant={'h6'} color="text.secondary">Created by {list.username}</Typography>
+              <Typography variant={'h5'}>Description: {list.description}</Typography>
+            </Card>
+          </Box>
+          <ul>
+            {list.movies.map(movie => {
+              return (
+                <li key={movie.id}>
+                  <MovieItem
+                    id={movie.id}
+                    title={movie.original_title}
+                    overview={movie.overview}
+                    poster={movie.poster_path}
+                    runtime={movie.runtime}
+                    release_date={movie.release_date}
+                    directed_by={movie.directed_by}
+                    cast={movie.cast}
+                  />
+                </li>
+              )
+            })}
+          </ul>
+        </>
       )}
     </div>
   )
