0953142ead485796837e059b1e6cb1564c245b07
Marcos Spomberg <marcosspomberg@gmail.com>
2022-07-28 21:24:09 -0400

    Wrap app around notistack component

--- src/App.js
+++ src/App.js
@@ -1,35 +1,38 @@
+import './App.scss';
 import Navbar from './components/Navbar/Navbar';
 import Home from './components/Home/Home';
 import List from './components/Lists/List';
 import NewList from './components/NewList/NewList'
 import useIndexData from './hooks/useIndexData';
 import EditList from './components/EditList/EditList';
-import './App.scss';
+import { SnackbarProvider } from 'notistack';
 import { Routes, Route } from 'react-router-dom';
 
 function App() {
   const indexData = useIndexData();
   return (
     <div className='app'>
-      <Navbar />
-      <Routes>
-        <Route
-          path='*'
-          element={<Home lists={indexData.lists} />}
-        />
-        <Route
-          path='lists/:listId'
-          element={<List />}
-        />
-        <Route
-          path='lists/new'
-          element={<NewList />}
-        />
-        <Route
-          path='lists/edit/:listId'
-          element={<EditList />}
-        />
-      </Routes>
+      <SnackbarProvider>
+        <Navbar />
+        <Routes>
+          <Route
+            path='*'
+            element={<Home lists={indexData.lists} />}
+          />
+          <Route
+            path='lists/:listId'
+            element={<List />}
+          />
+          <Route
+            path='lists/new'
+            element={<NewList />}
+          />
+          <Route
+            path='lists/edit/:listId'
+            element={<EditList />}
+          />
+        </Routes>
+      </SnackbarProvider>
     </div>
   );
 }
