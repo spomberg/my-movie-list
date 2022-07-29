e2a044765d561e0125c76f437ee2867949ae1b34
Marcos Spomberg <marcosspomberg@gmail.com>
2022-07-29 14:45:03 -0400

    Call useState and useEffect instead of importing hook

--- src/App.js
+++ src/App.js
@@ -3,13 +3,19 @@ import Navbar from './components/Navbar/Navbar';
 import Home from './components/Home/Home';
 import List from './components/Lists/List';
 import NewList from './components/NewList/NewList'
-import useIndexData from './hooks/useIndexData';
 import EditList from './components/EditList/EditList';
 import { SnackbarProvider } from 'notistack';
 import { Routes, Route } from 'react-router-dom';
+import { useEffect, useState } from 'react';
+import axiosConn from './axiosConn';
 
 function App() {
-  const indexData = useIndexData();
+  const [indexData, setIndexData] = useState({ lists: [] });
+  useEffect(() => {
+    Promise.resolve(axiosConn.get('/api/lists'))
+    .then(all => { setIndexData(prev => ({ ...prev, lists: all.data }))})
+  }, [])
+
   return (
     <div className='app'>
       <SnackbarProvider>
@@ -25,7 +31,10 @@ function App() {
           />
           <Route
             path='lists/new'
-            element={<NewList />}
+            element={<NewList 
+              setIndexData={setIndexData}
+              indexData={indexData}
+            />}
           />
           <Route
             path='lists/edit/:listId'
