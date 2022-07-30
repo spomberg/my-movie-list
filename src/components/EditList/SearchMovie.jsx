99ceeca29913f4c1001de4888f5e935270e62b9c
Marcos Spomberg <marcosspomberg@gmail.com>
2022-07-29 22:14:28 -0400

    Create SearchMovie modal files

---
+++ src/components/EditList/SearchMovie.jsx
@@ -0,0 +1,39 @@
+import './SearchMovie.scss';
+import Box from '@mui/material/Box';
+import Typography from '@mui/material/Typography';
+import Modal from '@mui/material/Modal';
+import axiosConn from '../../axiosConn';
+import { useNavigate } from 'react-router-dom';
+
+export default function SearchMovie(props) {
+  const navigate = useNavigate();
+  const style = {
+    position: 'absolute',
+    top: '50%',
+    left: '50%',
+    transform: 'translate(-50%, -50%)',
+    width: '60%',
+    height: '80%',
+    bgcolor: 'background.paper',
+    border: '2px solid #000',
+    boxShadow: 24,
+    p: 4,
+  };  
+
+  return (
+  <div>
+    <Modal
+        open={props.open}
+        onClose={props.handleClose}
+        aria-labelledby="modal-modal-title"
+        aria-describedby="modal-modal-description"
+      >
+        <Box sx={style}>
+          <Typography id="modal-modal-title" variant="h6" component="h2">
+            Add Movie
+          </Typography>
+        </Box>
+      </Modal>
+  </div>
+  )
+}
