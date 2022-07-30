ce3b4878932f3a2ae6d4fe780d3b65956375dadb
Marcos Spomberg <marcosspomberg@gmail.com>
2022-07-29 21:21:25 -0400

    Call useEffect to set up loading status

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,23 +1,43 @@
 import ListItem from "./ListItem"
 import "./Home.scss"
+import axiosConn from "../../axiosConn";
+import { BeatLoader } from "react-spinners";
+import { useEffect, useState } from "react";
+import { useSnackbar } from "notistack";
 
-export default function Home(props) {
+
+export default function Home() {
+  const [loading, setLoading] = useState(true);
+  const [lists, setLists] = useState([]);
+  const { enqueueSnackbar } = useSnackbar(); 
+
+  useEffect(() => {
+    Promise.resolve(axiosConn.get("/api/lists"))
+    .then(all => {
+      setLists(all.data);
+      setLoading(false);
+    })
+    .catch(err => enqueueSnackbar(err.message, { variant: 'error' }))
+  }, [enqueueSnackbar])
+
   return (
     <div className="index-lists">
-      <ul>
-        {props.lists.map(list => {
-        return(
-          <li key={list.id}>
-            <ListItem className="list-item"
-              id={list.id}
-              title={list.title}
-              username={list.username}
-              image={list.index_poster}
-            />
-          </li>
-        )
-        })}
-      </ul>
+      {loading ? (<BeatLoader className='loader' loading={loading} />) :
+        (<ul>
+          {lists.map(list => {
+          return(
+            <li key={list.id}>
+              <ListItem className="list-item"
+                id={list.id}
+                title={list.title}
+                username={list.username}
+                image={list.index_poster}
+              />
+            </li>
+          )
+          })}
+        </ul>)
+      }
     </div>
   )
 }
