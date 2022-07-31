f2482e6e39847481f56e1e0c7baa4be566c09606
Marcos Spomberg <marcosspomberg@gmail.com>
2022-07-31 14:08:45 -0400

    Move SearchMovie component to a separate folder

---
+++ src/components/SearchMovie/ResultsItem.jsx
@@ -0,0 +1,21 @@
+import './ResultsItem.scss';
+import Typography from '@mui/material/Typography';
+import AddCircleTwoToneIcon from '@mui/icons-material/AddCircleTwoTone';
+
+export default function ResultsItem(props) {
+  return (
+    <div className='results-item'>
+      <img 
+        src={props.poster_path ? `https://image.tmdb.org/t/p/original${props.poster_path}` : `https://res.cloudinary.com/djv3yhbok/image/upload/v1658030155/1665px-No-Image-Placeholder.svg_jgp6ma.png`} 
+        alt={`${props.title} poster`} 
+        onClick={() => props.onClick}
+      />
+      <Typography className='result-title' variant="subtitle1" >
+        {`${props.title}`}
+      </Typography>
+      <div className='add-movie'>
+        <AddCircleTwoToneIcon style={{ color: '3f50b5' }} />
+      </div>
+    </div>
+  )
+}
