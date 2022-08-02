a08359c19972092d729a533bcca8f18be25919c1
Marcos Spomberg <marcosspomberg@gmail.com>
2022-08-01 23:35:15 -0400

    Test isDuplicate helper function

--- src/helpers/helpers.test.js
+++ src/helpers/helpers.test.js
@@ -1,4 +1,4 @@
-import { moveElement } from "./helpers";
+import { moveElement, isDuplicate } from "./helpers";
 
 test("moveElement moves element up", () => {
   const mockArr = [1, 2, 3, 4];
@@ -6,4 +6,27 @@ test("moveElement moves element up", () => {
   const result = [1, 3, 2, 4];
   
   expect(input).toEqual(result);
+})
+
+
+test("isDuplicate returns true when the id is already in the list", () => {
+  const mockList = [
+    { id: 71407 },
+    { id: 87028 },
+    { id: 94901 }
+  ]
+  const input = isDuplicate(mockList, 94901);
+
+  expect(input).toEqual(true);
+})
+
+test("isDuplicate returns false when the id is not in the list", () => {
+  const mockList = [
+    { id: 71407 },
+    { id: 87028 },
+    { id: 94901 }
+  ]
+  const input = isDuplicate(mockList, 11111);
+
+  expect(input).toEqual(false);
 })
