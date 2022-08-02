70ba107a867dbe3c36ea6d93a82e1bf753c192c9
Marcos Spomberg <marcosspomberg@gmail.com>
2022-08-01 21:26:22 -0400

    Move onClick method to parent div

--- src/components/SearchMovie/ResultsItem.jsx
+++ src/components/SearchMovie/ResultsItem.jsx
@@ -4,11 +4,10 @@ import AddCircleTwoToneIcon from '@mui/icons-material/AddCircleTwoTone';
 
 export default function ResultsItem(props) {
   return (
-    <div className='results-item'>
+    <div className='results-item' onClick={() => props.onClick(props.id)} >
       <img 
         src={props.poster_path ? `https://image.tmdb.org/t/p/original${props.poster_path}` : `https://res.cloudinary.com/djv3yhbok/image/upload/v1658030155/1665px-No-Image-Placeholder.svg_jgp6ma.png`} 
         alt={`${props.title} poster`} 
-        onClick={() => props.onClick(props.id)}
       />
       <Typography className='result-title' variant="subtitle1" >
         {`${props.title}`}
