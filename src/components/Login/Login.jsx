5c47abe39f47399854964d67de81e5559cab3a82
Marcos Spomberg <marcosspomberg@gmail.com>
2022-08-05 09:00:45 -0400

    Add navigate to useEffect array dependency

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -33,7 +33,7 @@ export default function Login() {
         navigate('/');
       }
     })
-  }, [enqueueSnackbar])
+  }, [enqueueSnackbar, navigate])
   
   const handleSubmit = (event) => {
     event.preventDefault();
