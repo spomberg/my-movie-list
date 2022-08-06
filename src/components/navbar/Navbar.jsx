1863a2822e41197bcbc38c759b3f8a62b7cf6113
Marcos Spomberg <marcosspomberg@gmail.com>
2022-08-06 01:05:38 -0400

    Create new logged section and implement logout function

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -5,10 +5,38 @@ import PlaylistAddIcon from '@mui/icons-material/PlaylistAdd';
 import PersonIcon from '@mui/icons-material/Person';
 import PersonAddAltIcon from '@mui/icons-material/PersonAddAlt';
 import logo from '../../assets/logo.png'
+import Typography from '@mui/material/Typography';
+import Button from '@mui/material/Button';
+import axiosConn from "../../axiosConn";
+import { useSnackbar } from 'notistack';
 import { Link } from 'react-router-dom';
+import { useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
 
+export default function Navbar(props) {
+  const { enqueueSnackbar } = useSnackbar();
+  const navigate = useNavigate();
+  
+  useEffect(() => {
+    Promise.resolve(axiosConn.get('api/user'))
+    .then((res) => {
+      props.setUsername(res.data.username);
+    })
+  }, [props])
 
-export default function Navbar() {
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    Promise.resolve(axiosConn.put('api/logout'))
+    .then((res) => {
+      if (res.status === 204) {
+        props.setUsername('');
+        enqueueSnackbar('You logged out!', { variant: 'success' });
+        navigate('/');
+      } 
+    })
+    .catch((err) => enqueueSnackbar(err.message, { variant: 'error' }))
+  }
+
     return (
       <div className='navbar'>
         <img src={logo} alt='logo' />
@@ -23,14 +51,28 @@ export default function Navbar() {
           <Link to={'/lists/new'}><PlaylistAddIcon/>{`\nCREATE LIST`}</Link>
         </li>
       </ul>
-      <ul className='login-menu'>
+      {props.username ? (<ul className='logged'>
+        <li>
+        <Typography className='welcome-message' variant="h7">Welcome {props.username}!</Typography>
+        </li>
+        <li>
+        <Button 
+            variant="contained" 
+            type="submit"
+            onClick={handleSubmit}
+          >
+            LOGOUT
+          </Button>
+        </li>
+      </ul>) :
+      (<ul className='login-menu'>
         <li>
         <Link to={'/login'}><PersonIcon/>{`\nLOGIN`}</Link>
         </li>
         <li>
         <Link to={'/signup'}><PersonAddAltIcon/>{`\nSIGNUP`}</Link>
         </li>
-      </ul>
+      </ul>)}
       </div>
     )
 }
