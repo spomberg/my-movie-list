44cf9334db14e2f7a7e7038905a639148055e41b
Marcos Spomberg <marcosspomberg@gmail.com>
2022-08-05 22:41:05 -0400

    Add Signup route

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import List from './components/Lists/List';
 import NewList from './components/NewList/NewList'
 import EditList from './components/EditList/EditList';
 import Login from './components/Login/Login';
+import Signup from './components/Signup/Signup';
 import { SnackbarProvider } from 'notistack';
 import { Routes, Route } from 'react-router-dom';
 
@@ -33,6 +34,10 @@ function App() {
           <Route
             path='login'
             element={<Login />}
+          />
+          <Route
+            path='signup'
+            element={<Signup />}
           />
         </Routes>
       </SnackbarProvider>
