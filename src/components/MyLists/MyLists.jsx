29c9ffe069ea9d3180e2006eeb979a0f4e856cd5
Marcos Spomberg <marcosspomberg@gmail.com>
2022-08-09 17:35:18 -0400

    Setup useEffect to load list data

--- src/components/MyLists/MyLists.jsx
+++ src/components/MyLists/MyLists.jsx
@@ -1,7 +1,37 @@
 import './MyLists.scss';
+import ListItem from '../Home/ListItem';
+import Typography from '@mui/material/Typography';
+import axiosConn from '../../axiosConn';
+import { BeatLoader } from 'react-spinners';
+import { useEffect, useState } from 'react';
+import { useSnackbar } from 'notistack';
+import { useNavigate } from 'react-router-dom';
 
 export default function MyLists() {
+  const [loading, setLoading] = useState(true);
+  const [lists, setLists] = useState([]);
+  const { enqueueSnackbar } = useSnackbar();
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    Promise.resolve(axiosConn.get('api/user/lists'))
+    .then((res) => {
+      if (res.data.code === 200) {
+        setLists(res.data.list);
+        setLoading(false);
+      }
+      else {
+        enqueueSnackbar('Please login to view this page', { variant: 'info' })
+        navigate('/login')
+      }
+    })
+    .catch((err) => enqueueSnackbar(err.message, { variant: 'error' }))
+  }, [enqueueSnackbar])
+
   return (
-    <div>MyLists</div>
+    <div className='my-lists'>
+      {loading ? (<BeatLoader className='loader' loading={loading} />) : (
+      <Typography className='page-title' variant="h4">My Lists</Typography>)}
+    </div>
   )
 }
