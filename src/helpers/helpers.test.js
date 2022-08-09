b1a8c34c113e37ab5e27356620c47bce91a7107a
Marcos Spomberg <marcosspomberg@gmail.com>
2022-08-09 18:48:01 -0400

    Test removeMovie function

--- src/helpers/helpers.test.js
+++ src/helpers/helpers.test.js
@@ -1,4 +1,4 @@
-import { moveElement, isDuplicate } from "./helpers";
+import { moveElement, isDuplicate, removeMovie } from "./helpers";
 
 test("moveElement moves element up", () => {
   const mockArr = [1, 2, 3, 4];
@@ -29,4 +29,33 @@ test("isDuplicate returns false when the id is not in the list", () => {
   const input = isDuplicate(mockList, 11111);
 
   expect(input).toEqual(false);
+})
+
+test("removeMovie returns an array without the movie id entered", () => {
+  const mockList = [
+    { id: 71407 },
+    { id: 87028 },
+    { id: 94901 }
+  ]
+
+  const expectedOutput = [
+    { id: 71407 },
+    { id: 94901 }
+  ]
+
+  const input = removeMovie(mockList, 87028);
+
+  expect(input).toEqual(expectedOutput);
+})
+
+test("removeMovie returns an identical array if the movie id is not present", () => {
+  const mockList = [
+    { id: 71407 },
+    { id: 87028 },
+    { id: 94901 }
+  ]
+
+  const input = removeMovie(mockList, 11111);
+
+  expect(input).toEqual(mockList);
 })
